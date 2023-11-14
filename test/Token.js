a07f59477bf04774a2ebfc7b4a828def725f949d
Vladimir Ten <sunrostern@gmail.com>
2023-11-14 10:16:13 -0700

    Token Balance

--- test/Token.js
+++ test/Token.js
@@ -1,24 +1,47 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
 
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), 'ether');
+}
+
 describe('Token Contract', () => {
-  let TokenContract;
+  let accounts, deployer, TokenContract;
   beforeEach(async () => {
     // Constructing the Token Factory.
     const TokenContractFactory = await ethers.getContractFactory('Token');
 
     // Deploying the Token Factory to the blockchain.
-    TokenContract = await TokenContractFactory.deploy();
+    TokenContract = await TokenContractFactory.deploy('Token', 'TOK', 1000000);
+
+    accounts = await ethers.getSigners();
+    deployer = accounts[0].address;
   });
-  it('Has a correct name.', async () => {
-    expect(await TokenContract.name()).to.equal('Token');
-  })
 
-  it('Has a correct symbol.', async () => {
-    expect(await TokenContract.symbol()).to.equal('TOK');
-  })
+  describe('Deployment', () => {
+    const name = 'Token';
+    const symbol = 'TOK';
+    const decimals = 18;
+    const totalSupply = tokens('1000000');
 
-  it('Has a correct decimals value.', async () => {
-    expect(await TokenContract.decimals()).to.equal(18);
-  })
-})
+    it('Has a correct name.', async () => {
+      expect(await TokenContract.name()).to.equal(name);
+    });
+
+    it('Has a correct symbol.', async () => {
+      expect(await TokenContract.symbol()).to.equal(symbol);
+    });
+
+    it('Has a correct decimals value.', async () => {
+      expect(await TokenContract.decimals()).to.equal(decimals);
+    });
+
+    it('Has a correct total supply.', async () => {
+      expect(await TokenContract.totalSupply()).to.equal(totalSupply);
+    });
+
+    it('Assigns total supply to a deployer.', async () => {
+      expect(await TokenContract.balanceOf(deployer)).to.equal(totalSupply);
+    });
+  });
+});
